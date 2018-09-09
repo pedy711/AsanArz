548043d80c0d5ce046e6efc7f1d7441ee298183f
pedram <pedram.khoshdani@gmail.com>
2018-09-09 23:40:59 +0200

    initial commit

---
+++ AsanArzServer/src/main/java/com/pco/asanarz/server/ServerMain.java
@@ -0,0 +1,14 @@
+package com.pco.asanarz.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMain {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ServerMain.class, args);
+	}
+}
+
+
