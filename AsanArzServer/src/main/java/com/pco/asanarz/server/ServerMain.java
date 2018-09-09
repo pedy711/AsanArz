b8d009d9641b450e47affde072f62a2f7abca642
pedram <pedram.khoshdani@gmail.com>
2018-09-10 01:26:14 +0200

    Database and server are set up and running

--- AsanArzServer/src/main/java/com/pco/asanarz/server/ServerMain.java
+++ AsanArzServer/src/main/java/com/pco/asanarz/server/ServerMain.java
@@ -2,8 +2,10 @@ package com.pco.asanarz.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages="com.pco.asanarz")
 public class ServerMain {
 
 	public static void main(String[] args) throws Exception {
